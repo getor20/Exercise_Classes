215ef6f8233304a11fdd6d27dc54e06ef8fefdc6
getor20 <ggetor20@gmail.com>
2024-08-07 16:58:18 +0200

    Добавьте файлы проекта.

---
+++ Exercise_Classes/Child.cs
@@ -0,0 +1,23 @@
+﻿internal class Child
+{
+    
+    private string name;
+    private string surname;
+    private int age;
+
+    
+    public Child(string name, string surname, int age)
+    {
+        this.name = name;
+        this.surname = surname;
+        this.age = age;
+    }
+
+    
+    public void ShowInfo()
+    {
+        Console.WriteLine($"Имя: {name}");
+        Console.WriteLine($"Фамилия: {surname}");
+        Console.WriteLine($"Возраст: {age}");
+    }
+}
---
+++ Exercise_Classes/Program.cs
@@ -0,0 +1,16 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+       
+        Child child1 = new Child("Иван", "Иванов", 10);
+        Child child2 = new Child("Мария", "Петрова", 8);
+
+        
+        Console.WriteLine("Информация о первом ребенке:");
+        child1.ShowInfo();
+
+        Console.WriteLine("\nИнформация о втором ребенке:");
+        child2.ShowInfo();
+    }
+}
